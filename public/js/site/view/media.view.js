5cb08cd99ced399cefea74d815d93b957bd3b3c2
Robert Velton <rvelton@illuminateed.com>
2017-06-30 17:22:57 +0000

    Updating the media pages.

--- public/js/site/view/media.view.js
+++ public/js/site/view/media.view.js
@@ -12,7 +12,32 @@ define([
             });
         },
 
-        initialize: function() { },
+        initialize: function() { 
+            console.log('Initializing Media Page');
+            
+            var image_arr = $('.image-row .col-sm-3 img'),
+                image_counter = 0;
+                
+            $('#image-flipper').attr('src', $(image_arr[0]).attr('src').replace('/thumbs', ''));
+            setInterval(function() {
+                image_counter++;
+        
+                if (image_counter > image_arr.length) {
+                    image_counter = 0;
+                }
+                var image_src = $(image_arr[image_counter]).attr('src').replace('/thumbs', '');
+                $('#image-flipper').attr('src', image_src);    
+            }, 5000);
+        
+            $('.image-row .col-sm-3 img').on('click', function(ev) {
+                var image_src = $(ev.currentTarget).attr('src').replace('/thumbs', ''),
+                    image_title = $(ev.currentTarget).attr('title');
+        
+                $('#picture-modal .modal-title').html(image_title);
+                $('#picture-modal .modal-body').html('<img src="' + image_src + '" />')
+                $('#picture-modal').modal('show');
+            });
+        },
 
         render: function() { },
 
